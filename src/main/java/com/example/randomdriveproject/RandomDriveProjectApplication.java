1c3d4d3c7cec4ef193b33b97bf8e674ffeafe7ce
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2023-10-11 16:02:53 +0900

    Initial commit

---
+++ src/main/java/com/example/randomdriveproject/RandomDriveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.randomdriveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomDriveProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomDriveProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/randomdriveproject/RandomDriveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomdriveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomDriveProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
