476c97b48ea54297cc7e375947d863b6b8ef7e53
hms <mshan2923@gmail.com>
2023-10-12 11:37:51 +0900

    feat : #3 로그인 기능 - 카카오 로그인 후 회원 정보 가져오기

--- src/main/java/com/example/randomdriveproject/entity/User.java
+++ src/main/java/com/example/randomdriveproject/entity/User.java
@@ -3,9 +3,9 @@ package com.example.randomdriveproject.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
@@ -15,7 +15,7 @@ import java.util.List;
 //@Setter
 @Table(name = "user")
 @NoArgsConstructor
-public class User implements UserDetails {
+public class User{// implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,38 +27,38 @@ public class User implements UserDetails {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Override//권한 반환
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("user"));
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+//    @Override//권한 반환
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of(new SimpleGrantedAuthority("user"));
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return this.password;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return this.username;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
 }
--- src/main/java/com/example/randomdriveproject/service/UserService.java
+++ src/main/java/com/example/randomdriveproject/service/UserService.java
@@ -1,11 +1,13 @@
 package com.example.randomdriveproject.service;
 
-import com.example.randomdriveproject.dto.KakaoDto;
+import com.example.randomdriveproject.dto.kakao.KakaoAccount;
+import com.example.randomdriveproject.dto.kakao.KakaoDto;
 import com.example.randomdriveproject.dto.KakaoTokenDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysql.cj.xdevapi.JsonParser;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONObject;
+
+import org.springframework.boot.jackson.JsonObjectDeserializer;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,17 +17,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+
 
 @Slf4j(topic = "UserService")
 @Service
 public class UserService {
 
     private final static String KAKAO_AUTH_URI = "https://kauth.kakao.com";
+    private final static String KAKAO_API_URI = "https://kapi.kakao.com";
 
     public  String getKaKaoLogin()
     {
@@ -33,6 +34,33 @@ public class UserService {
                 "?response_type=code" +
                 "&client_id=0c3c82e2bab1baa630c741b2c9f72e3c" +
                 "&redirect_uri=http://localhost:8080/api/auth/login";
+    }
+    public static Map<String, Object> toMap(String jsonString) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonString, Map.class);
+    }
+    public static String getProperty(String json)
+    {
+        //{nickname=이름} 이렇게 들어옴
+        //kakao_account 도
+
+        String content = json.substring(1, json.length() - 1);
+
+        return  content.split("=")[1];
+    }
+    public static Map<String, String> toMap_Property(String json)
+    {
+        Map<String, String> map = new HashMap<>();
+        for (String pair : json.split(",")) {
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                String key = keyValue[0].trim();
+                String value = keyValue[1].trim();
+                map.put(key, value);
+            }
+        }
+
+        return map;
     }
 
     public KakaoDto getKakaoInfo(String code) throws Exception
@@ -64,12 +92,13 @@ public class UserService {
 
 
             {
-//                var map = toMap(response.getBody());
-//                log.info(map.get("access_token").toString() + "/" + map.get("refresh_token").toString());
 
                 var temp = new KakaoTokenDto(response.getBody());
                 log.info(temp.getAccess_token() + " / " + temp.getRefresh_token() + " / " + temp.getExpires_in());
                 //키값은 KakaoTokenDto 참고
+
+                accessToken = temp.getAccess_token();
+                refreshToken = temp.getRefresh_token();
             }
 
 
@@ -79,7 +108,26 @@ public class UserService {
             throw  new Exception("API call failed" + e);
         }
 
-        return null;
+        return getUserInfoWithToken(accessToken);
     }
 
+    private KakaoDto getUserInfoWithToken(String accessToken) throws Exception
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+
+        //HttpHeader 담기
+        RestTemplate rt = new RestTemplate();
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = rt.exchange(
+                KAKAO_API_URI + "/v2/user/me",
+                HttpMethod.POST,
+                httpEntity,
+                String.class
+        );
+
+        return new KakaoDto(response);
+    }
 }
