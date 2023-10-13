d40451c4796faee470b069180a1ec4d4fed86978
haechanlee96 <131975479+haechanlee96@users.noreply.github.com>
2023-10-13 12:20:29 +0900

    feat : #8 기본 프론트 구성
    동작 테스트용 간단한 프론트 화면 구성

--- src/main/java/com/example/randomdriveproject/controller/HomeController.java
+++ src/main/java/com/example/randomdriveproject/controller/HomeController.java
@@ -13,13 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class HomeController {
     private final UserService userService;
+
     @GetMapping("/")
-    public String hello(@CookieValue(required = false, value = "Authorization_Access")String access,
+    public String hello(@CookieValue(required = false, value = "Authorization_Access") String access,
                         Model model,
                         HttpServletResponse response) throws Exception {
         var user = userService.getUserInfoWithToken(access, response);
 
         model.addAttribute("nickname", user.getKakao_account().getProfile().getNickname());
         return "index";
+
+    }
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("message", "안녕하세요!");
+        return "index"; // 뷰 이름은 templates 폴더에 있는 템플릿 파일명과 일치해야 합니다.
+    }
+
+    @GetMapping("/navigation")
+    public String showNavigation() {
+        return "navigation";
     }
 }
--- src/main/java/com/example/randomdriveproject/controller/RouteController.java
+++ src/main/java/com/example/randomdriveproject/controller/RouteController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -23,7 +24,7 @@ public class RouteController {
     @GetMapping("/map")
     public String showMap(Model model) {
         model.addAttribute("kakaoJavascriptApiKey", kakaoJavascriptApiKey);
-        return "index";
+        return "navigation";
     }
 
     @GetMapping("/route")
