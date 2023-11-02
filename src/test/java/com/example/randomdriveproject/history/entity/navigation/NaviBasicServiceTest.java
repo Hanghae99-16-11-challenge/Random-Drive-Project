ba520f2098c44a3e46dd2eefde7161dc7aad9cf1
hms <mshan2923@gmail.com>
2023-11-02 20:40:19 +0900

    feat : #23 테스트 코드 - basic.Navi

--- src/test/java/com/example/randomdriveproject/history/entity/navigation/NaviBasicServiceTest.java
+++ src/test/java/com/example/randomdriveproject/history/entity/navigation/NaviBasicServiceTest.java
@@ -9,6 +9,7 @@ import com.example.randomdriveproject.request.service.KakaoKeywordSearchService;
 import com.example.randomdriveproject.request.service.KakaoUriBuilderService;
 import com.example.randomdriveproject.request.service.RandomKakaoCategorySearchService;
 import com.example.randomdriveproject.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,8 +36,16 @@ public class NaviBasicServiceTest {
     @MockBean
     UserRepository userRepository;
     //    @Autowired
-//    @MockBean//가상 객체
-//            RandomDestinationRepository randomDestinationRepository;
+
+    String[] TestAddress = {"제주특별자치도 제주시 남성로 2",
+            "제주특별자치도 제주시 남성로 22길2",
+            "제주특별자치도 제주시 조천읍 와산서1길 32",
+            "제주특별자치도 제주시 애월읍 고내로9길 25"};
+
+    // [[x,y]...[x,y]]
+    double[][] waypoint = {{126.53001226699159, 33.50674031248494},
+            {126.51580214859632, 33.504461199016994},
+            {126.49423334018748, 33.48096748115865}};
 
     KeywordSearchService keywordSearchService;
     KakaoRouteSearchService kakaoRouteSearchService;
@@ -64,16 +73,47 @@ public class NaviBasicServiceTest {
         ReflectionTestUtils.setField(kakaoCategorySearchService,
                 "kakaoRestApiKey",
                 kakaoRestApiKey);//@Vaild 가 작동이 안되 , 리플렉션으로 넣어줌 + (kakaoRouteSearchService안)
+
+        ReflectionTestUtils.setField(kakaoKeywordSearchService,
+                "kakaoRestApiKey",
+                kakaoRestApiKey);//@Vaild 가 작동이 안되 , 리플렉션으로 넣어줌 + (keywordSearchService 안)
+        
         ReflectionTestUtils.setField(kakaoRouteSearchService,
                 "kakaoRestApiKey",
                 kakaoRestApiKey);//@Vaild 가 작동이 안되 , 리플렉션으로 넣어줌
 
     }
     
-    //@DisplayName("")//설명 없음
+    @DisplayName("키워드 To 주소")
     @Test
     void requestKeywordRandomWay()
     {
-        keywordSearchService.requestKeywordRandomWay("카카오");
+        var result = keywordSearchService.requestKeywordRandomWay("카카오");
+
+        Assertions.assertNotNull(result);
+        Assertions.assertFalse(result.isEmpty());
+    }
+
+
+    //KakaoRouteSearchService
+
+    @DisplayName("길 생성")
+    @Test
+    void requestRoute()
+    {
+        var result = kakaoRouteSearchService.requestRouteSearch(TestAddress[0], TestAddress[1]);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.getRoutes().length > 0);
+    }
+
+    @DisplayName("길 재생성")
+    @Test
+    void requestRoute_ReSearch()
+    {
+        var result = kakaoRouteSearchService.requestRouteReSearch(waypoint[0][1], waypoint[0][0]);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(result.getRoutes().length > 0);
     }
 }
