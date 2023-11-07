22f8aec8be69c583d0833cffcfe99bcec0dc96bf
hms <mshan2923@gmail.com>
2023-11-07 21:47:30 +0900

    Revert "Merge branch 'fix/infi-reset'"
    This reverts commit b7762cc00e7f09212236051f170872d065510eb2, reversing
    changes made to f9d6395c829e67be3b34b34dd84fd5a61e3e19b3.

--- src/main/resources/static/js/calculation.js
+++ src/main/resources/static/js/calculation.js
@@ -87,7 +87,7 @@ function updateMap(mouseEvent)
 
 function update_refact(lat, lng)
 {
-    EditMark(positionMark, positionText, lat, lng, '현위치');
+    EditMark(positionMark, positionText, lat, lng, '현재 위치');
 
     if(routeData == null)
     {
@@ -369,7 +369,6 @@ function stopNavi()
     naviInfo_GuidIndex = 1;
     naviInfo_ProcessIndex = 1;
     naviInfo_State = -1;
-    readyToStart = false;
 
     positionMark.setMap(null);
     positionText.close();
@@ -387,7 +386,6 @@ function clearNavi()
     naviInfo_SectionIndex = 0;
     naviInfo_GuidIndex = 1;
     naviInfo_ProcessIndex = 1;
-    readyToStart = false;
 
     positionMark.setMap(null);
     positionText.close();
--- src/main/resources/static/js/navi.js
+++ src/main/resources/static/js/navi.js
@@ -521,9 +521,6 @@ function onClick_StartNavi_navi() {
 
         map.setLevel(3, {animate: true});// 사용시 보이는 위치 달라짐
         panTo(startPoint.y, startPoint.x);
-
-        //startNavi();
-        startCorutine();
     }
 
 }
@@ -556,15 +553,6 @@ function Update_GuidIndo_navi() {
     document.getElementById('guid_left').innerText
         = "도착까지  " + visibilityDistance(pathLeftDistance) + " / " + visibilityTime(pathLeftDuration);
 
-}
-function Update_GuidInfo_NotStart()
-{
-    let nextGuid = routeData.guides[naviInfo_ProcessIndex];
-
-    document.getElementById('guid_ance').innerText
-        = "전방 " + visibilityDistance(nextGuidDistacne) + " 에서\n" + nextGuid.guidance;
-    document.getElementById('guid_left').innerText
-        = " '시작점에 도착 하지 않았습니다.' ";
 }
 
 function visibilityDistance(dis = 0) {
